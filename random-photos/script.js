0cbcc1a7d63929d77329ec22c0dead7e24f32924
Howard Huang <blackhuang0121@gmail.com>
2025-07-03 17:53:51 +0800

    Add random photos from img folder v2

--- random-photos/script.js
+++ random-photos/script.js
@@ -1,32 +1,12 @@
-const imageList = [
-    "img/DSCF2002.JPG",
-    "img/DSCF2117.JPG",
-    "img/DSCF2112.JPG",
-    "img/DSCF2125.JPG",
-    "img/DSCF2161.JPG",
-    "img/DSCF2274.JPG"
-];
-
-const imageContainerEl = document.querySelector('.image-container');
-const btnEl = document.querySelector('.btn');
-let currentIndex = 0;
-const imagesPerClick = 4;  // 每次新增幾張
-
-btnEl.addEventListener('click', addNewImages);
-
-function addNewImages() {
-    const nextIndex = Math.min(currentIndex + imagesPerClick, imageList.length);
-    for (let i = currentIndex; i < nextIndex; i++) {
-        const newImgEl = document.createElement('img');
-        newImgEl.src = imageList[i];
-        imageContainerEl.appendChild(newImgEl);
-    }
-    currentIndex = nextIndex;
-    if (currentIndex >= imageList.length) {
-        btnEl.disabled = true;
-        btnEl.textContent = '沒有更多照片了';
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 }
-
-// 預設載入第一批
-addNewImages();
+const imageList = [];
+for (let i = 1; i <= 20; i++) {
+    imageList.push(`img/photo${i}.jpg`);
+}
+shuffle(imageList);
+// 然後就用「每次加 N 張，直到沒照片」的寫法
